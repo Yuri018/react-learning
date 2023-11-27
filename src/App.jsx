0f80d667c376f7504b101f4ca4dd4b052b192eff
Juri Ryzhkov <plktmn@gmail.com>
2023-11-27 17:36:28 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,11 @@
-import './App.css';
-import Button from './components/Button/Button';
-
-export const animal = "Dog";
+import "./App.css";
+import Button from "./components/Button";
+import Lesson18 from "./lessons/Lesson18";
 
 function App() {
-  // JSX позволят нам создавать переменные и передавать туда теги
-  const name = <p>My name is Juri</p>;
-  const job = "Frontend developer";
-  const tegJobId = "Job";
-
-  const characters = {
-    hair: "black",
-    height: "185",
-  };
-
-  const sum = (a, b) => a + b;
-
-  const numberOfFriends = (count) => {
-    if (count > 5) {
-      return `У вас ${count} друзей`;
-    }
-  };
-
-  const rich = true;
-
   return (
     <div className="App">
-      {name}
-      {/* JSX позволяет нам передавать переменные с помощью фигурных скобок в HTML дерево */}
-      <p id={tegJobId}>I am {job}</p>
-      <p>Возвращаемая функция: {sum(2, 2)}</p>
-      <p>Характеристики</p>
-      <ul>
-        <li>Цвет волос: {characters.hair} </li>
-        <li>Рост: {characters.height} </li>
-      </ul>
-      <p>Favorite animal: {animal}</p>
-      <div> {numberOfFriends(7)} </div>
-      <div>{rich ? "Yeu" : "No"}</div>
-
-      <div className="button-container">
-        <Button name="Отправить" type="submit" />
-      </div>
-      <div className="button-container">
-        <Button name="Получить" />
-      </div>
-      <div className="button-container">
-        <Button />
-      </div>
-      <div className="button-container">
-        <Button />
-      </div>
+      <Lesson18 />
     </div>
   );
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import "./styles.css";
 
-function Button({ name = "Send", type = "button" }) {
+function Button({ name = "Send", type = "button", onClick }) {
   console.log(name);
   return (
-    <button className="button-component" type={type}>
+    <button onClick={onClick} className="button-component" type={type}>
       {name}
     </button>
   );
