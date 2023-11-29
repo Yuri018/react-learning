92cd6f481349f8c900227841717faa863153d11b
Juri Ryzhkov <plktmn@gmail.com>
2023-11-29 11:29:59 +0100

    finalizing homework 19

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Button from "./components/Button";
+import HomeWork19 from "./homeWorks/HomeWork19";
 // import Lesson18 from "./lessons/Lesson18";
 // import Counter from "./components/Counter/Counter";
 import Lesson19 from './lessons/Lesson19';
@@ -9,9 +10,10 @@ function App() {
     <div className="App">
       {/* <Lesson18 /> */}
       {/* <Counter /> */}
-      <Lesson19 />
+      {/* <Lesson19 /> */}
       {/* All our HomeWorks */}
       {/* <HomeWork18 /> */}
+      <HomeWork19 />
     </div>
   );
 }
