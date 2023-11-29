88fc2699b26694bdc90c6d458d8f198bc7c118fb
Juri Ryzhkov <plktmn@gmail.com>
2023-11-29 09:38:49 +0100

    add variant homeWork19

---
+++ src/homeWorks/HomeWork19/HomeWork19.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Button from "../../components/Button";
+import "./styles.css";
+
+function HomeWork19() {
+
+    const porscheCar = {
+      brand: "Porsche",
+      model: "911",
+      year: 2023,
+      color: "Silver",
+      price: 120000,
+      isAvailable: true,
+    };
+
+    const [car, setCar] = useState();
+
+    return
+}
+export default HomeWork19;
